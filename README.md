552126c3e3fb90c2d578c422a3e05438328a74cb
ikhsanfd1 <dilah80999@gmail.com>
2024-03-01 13:54:44 +0700

    Initialized project

---
+++ README.md
@@ -0,0 +1,8 @@
+# Getting Started
+
+1. npm install
+2. node index.js 25.0 5.0
+   25.0 = (Waktu bekerja 25 Menit)
+   5.0 = (Waktu istirahat 5 Menit)
+
+# For stop ctrl + c
---
+++ index.js
@@ -0,0 +1,45 @@
+const notifier = require('node-notifier');
+const moment = require('moment');
+const argTime = process.argv.slice(2);
+
+const POMODORO_DURATION = argTime[0];
+const BREAK_DURATION = argTime[1];
+
+let isWorking = false;
+let remainingTime = 0;
+
+function formattingTime(totalSecond) {
+  const duration = moment.duration(totalSecond, 'seconds');
+  const hours = duration.hours().toString().padStart(2, '0');
+  const minutes = duration.minutes().toString().padStart(2, '0');
+  const seconds = duration.seconds().toString().padStart(2, '0');
+
+  return `${hours}:${minutes}:${seconds}`;
+}
+
+function startTimer(duration) {
+  isWorking = !isWorking;
+  remainingTime = duration * 60;
+
+  const timer = setInterval(() => {
+    remainingTime--;
+
+    const formattedTime = formattingTime(remainingTime);
+
+    console.log(`${isWorking ? 'Work' : 'Break'}: ${formattedTime}`);
+
+    if (remainingTime === 0) {
+      clearInterval(timer);
+      notifier.notify({
+        title: isWorking ? 'Istirahat 5 menit' : 'Bekerja 25 menit',
+        message: isWorking ? 'You must a break 5 seconds!' : 'Work hard again!',
+        sound: true,
+        wait: true,
+      });
+
+      startTimer(isWorking ? BREAK_DURATION : POMODORO_DURATION);
+    }
+  }, 1000);
+}
+
+startTimer(POMODORO_DURATION);
